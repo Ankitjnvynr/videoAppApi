65e3db96d1c317fe9eab72b96c313a9feee23724
ankitjnvynr <ankitbkana@outlook.com>
2025-04-01 22:38:04 +0530

    added the utils

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 
 import express from 'express'
 
+import cors from "cors"
+import cookieParser from 'cookie-parser';
 
-export const app = express();
+
+const app = express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:'16kb'}))
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -36,5 +52,7 @@ app.get('/api/user', (req, res) => {
         ]
     })
 })
+
+export {cors}
 
 
